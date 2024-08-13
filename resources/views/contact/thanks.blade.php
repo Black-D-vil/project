ac81d46a85a2b13ffcee6e457af372d127469338
Black-D-vil <asaduzzamanhridoyice@gmail.com.com>
2024-08-13 14:38:28 +0600

    first commit

---
+++ resources/views/contact/thanks.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+@section('content')
+  
+    <div class="card">
+        <div class="card-header">Contact Form</div>
+        <div class="card-body"> 
+           <h2> Thanks You guys!!!!!!! </h2>
+        </div>
+    </div>
+
+@stop
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3 6.0/dist/jquery.slim.min.js"></script> -->
+        <!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script> -->
+        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>  -->
+    </head> 
+    <body> 
+        <div class="container">
+
+            @yield('content') 
+
+            <div class="card bg-secondary text-white"> 
+                <div class="card-body">Copyright (c) Aasduzzaman Hridoy</div> 
+            </div> 
+        </div> 
+    </body> 
+    </html>
